32af89bab0374e069aaf89347dec07bb5d8d71d0
Isaac2036 <isaacpablo0596@hotmail.com>
2023-10-08 07:44:54 -0700

    Write a function that adds 2 integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,13 +3,23 @@
 
 
 def add_integer(a, b=98):
-    """ add two integers or flots """
+	
+  """Adds two integers.
+  Args:
+    a: The first integer.
+    b: The second integer. Defaults to 98.
+  Returns:
+    The sum of a and b.
+  Raises:
+    TypeError: If a or b is not an integer or float.
+  """	
+
 	if a != a:
-		a = 89
+	    a = 89
 	if b != b:
-        	b = 89
+            b = 89
 	if a is None or (type(a) is not int and type(a) is not float):
-		raise TypeError("a must be an integer")
+	    raise TypeError("a must be an integer")
 	if type(b) is not int and type(b) is not float:
-		raise TypeError("b must be an integer")
+	    raise TypeError("b must be an integer")
 	return int(a) + int(b)
