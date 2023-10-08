8322c86d00a8f3d9318c4201651865b935973aa9
Isaac2036 <isaacpablo0596@hotmail.com>
2023-10-08 07:27:46 -0700

    Write a function that adds 2 integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 """Defines an integer addition function."""
-def add_integer(a, b=98):
-  """Adds two integers.
-  Args:
-    a: The first integer.
-    b: The second integer. Defaults to 98.
-  Returns:
-    The sum of a and b.
-  Raises:
-    TypeError: If a or b is not an integer or float.
-  """
 
 def add_integer(a, b=98):
     """ add two integers or flots """
