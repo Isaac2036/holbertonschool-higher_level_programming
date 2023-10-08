438e786fe3f1c0a1051497ed7459086aa5608d04
Isaac2036 <isaacpablo0596@hotmail.com>
2023-10-08 10:21:28 -0700

    Write a class Square that defines a square by: (based on 2-square.py)

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-# 3-square.py
 
-"""Define a class Square."""
+
+"""Example Google style docstrings."""
+
 
 class Square:
-    """Represent a square."""
+    """The summary line for a class docstring should fit on one line."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
+        """The summary line for a class docstring should fit on one line."""
+        if type(size) == int:
+            if size >= 0:
+                self._size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """The summary line for a class docstring should fit on one line."""
+        return (self._size * self._size)
