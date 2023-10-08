fc0da9abbd7b8f887c48a7e7c084c0b50c267f4b
Isaac2036 <isaacpablo0596@hotmail.com>
2023-10-08 10:29:35 -0700

    Write a class Square that defines a square by: (based on 4-square.py)

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python3
-
-
-"""Example Google style docstrings."""
+# 5-square.py
+"""Define a class Square."""
 
 
 class Square:
-    """The summary line for a class docstring should fit on one line."""
+    """Represent a square."""
 
-    def __init__(self, size=0):
-        """The summary line for a class docstring should fit on one line."""
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
-
-    def area(self):
-        """The summary line for a class docstring should fit on one line."""
-        return (self._size * self._size)
 
     @property
     def size(self):
-        """The summary line for a class docstring should fit on one line."""
-        return self._size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """The summary line for a class docstring should fit on one line."""
-        if type(value) == int:
-            if value >= 0:
-                self._size = value
-            else:
-                raise ValueError("size must be >= 0")
-        else:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
