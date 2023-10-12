480021a18ce98afae9cbf532295cdd575d41777e
Isaac2036 <isaacpablo0596@hotmail.com>
2023-10-11 21:50:35 -0700

    Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Este modulo define una clase
+"""
+class Rectangle:
+    """
+    Esta clase define un rectángulo
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Método constructor, recibe el ancho y alto
+        Args:
+            width (int): Ancho del rectángulo.
+            height (int): Alto del rectángulo
+        """
+        self.__height = height
+        self.__width = width
+    @property
+    def height(self):
+        """
+        Este atributo regresa el ancho del rectángulo
+        Returns:
+            int: Retorna el ancho(height)
+        """
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """
+        Este atributo configura el ancho del rectángulo.
+        Args:
+            value (int): Este valor configura el ancho del rectágulo.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    @property
+    def width(self):
+        """
+        Este atributo configura el ancho del rectángulo.
+        Args:
+            value (int): Este valor configura el ancho del rectágulo.
+        """
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """Getter & Setter for the attribute width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+
+
+
+
+
+
+
