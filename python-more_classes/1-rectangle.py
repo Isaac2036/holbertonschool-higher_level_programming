a557a354d5e554f7cd71c7968b1da1d02c639c12
Isaac2036 <isaacpablo0596@hotmail.com>
2023-10-11 22:04:08 -0700

    Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -2,8 +2,6 @@
 """
 Este modulo define una clase
 """
-
-
 class Rectangle:
     """
     Esta clase define un rectángulo
@@ -18,11 +16,27 @@ class Rectangle:
         self.__height = height
         self.__width = width
     @property
+    def width(self):
+        """
+        Este atributo configura el ancho del rectángulo.
+        Args:
+            value (int): Este valor configura el ancho del rectágulo.
+        """
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """Getter & Setter for the attribute width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    @property
     def height(self):
         """
-        Este atributo regresa el alto del rectángulo.
+        Este atributo regresa el ancho del rectángulo
         Returns:
-            int: Retorna el alto(height)
+            int: Retorna el ancho(height)
         """
         return self.__height
     @height.setter
@@ -37,26 +51,9 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    @property
-    def width(self):
-        """
-        Este atributo configura el ancho del rectángulo.
-        Args:
-            value (int): Este valor configura el ancho del rectágulo(width).
-        """
-        return self.__width
-    @width.setter
-    def width(self, value):
-        """
-        Este atributo configura el valor de __height
-        Args:
-            value (int): Este valor configura el alto del rectángulo.
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+
+
+
 
 
 
