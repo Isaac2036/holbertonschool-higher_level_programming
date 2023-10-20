b4d0b2178a9479207f18fab3c89cc2e71d6f1689
isaac2036 <isaacpablo0596@hotmail.com>
2023-10-20 01:05:55 -0700

    Feat:Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,68 +1,42 @@
 #!/usr/bin/python3
-"""
-Este modulo define una clase
-"""
+"""Defines a Rectangle class."""
+
+
 class Rectangle:
-    """
-    Esta clase define un rectángulo
-    """
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
-        """
-        Método constructor, recibe el ancho y alto
+        """Initialize a new Rectangle.
+
         Args:
-            width (int): Ancho del rectángulo.
-            height (int): Alto del rectángulo
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
-        self.__height = height
-        self.__width = width
+        self.width = width
+        self.height = height
+
     @property
     def width(self):
-        """
-        Este atributo regresa el ancho del rectángulo
-        Returns:
-            int: Retorna el ancho(width)
-        """
+        """Get/set the width of the rectangle."""
         return self.__width
+
     @width.setter
     def width(self, value):
-        """
-        Este atributo configura el ancho del rectángulo.
-        Args:
-            value (int): Este valor configura el ancho del rectágulo.
-        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
-        """
-        Este atributo regresa el alto del rectángulo
-        Returns:
-            int: Retorna el alto(height)
-        """
+        """Get/set the height of the rectangle."""
         return self.__height
+
     @height.setter
     def height(self, value):
-        """
-        Este atributo configura el valor de __height
-        Args:
-            value (int): Este valor configura el alto del rectángulo.
-        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-
-
-
-
-
-
-
-
-
-
